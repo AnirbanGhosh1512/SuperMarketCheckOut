e62487f7b5056ff9aed3371f9945a221bd32cf99
AnirbanGhosh1512 <anirban.ghosh1512@gmail.com>
2023-01-08 19:55:42 +0530

    Added CheckOut and ItemList.

---
+++ SuperMarketCheckOut/SuperMarketCheckoutRules/Checkout.cs
@@ -0,0 +1,40 @@
+﻿using SuperMarketCheckOut.SuperMarketItemList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMarketCheckOut.SuperMarketCheckoutRules
+{
+    /// <summary>
+    /// This class is responsible for the define the rules of the check
+    /// out of the supermarket system. @Author: Anirban Ghosh
+    /// </summary>
+
+    class Checkout
+    {
+        private Item[] items;
+
+        public Checkout()
+        {
+            items = new Item[0];
+        }
+
+        public void Scan(Item item)
+        {
+            Array.Resize(ref items, items.Length + 1);
+            items[items.Length - 1] = item;
+        }
+
+        public decimal Total()
+        {
+            decimal totalPrice = 0;
+            foreach (Item item in items)
+            {
+                totalPrice += item.PricingStrategy.CalculatePrice(1);
+            }
+            return totalPrice;
+        }
+    }
+ }
---
+++ SuperMarketCheckOut/SuperMarketItemList/Item.cs
@@ -0,0 +1,19 @@
+﻿using SuperMarketCheckOut.Common;
+
+namespace SuperMarketCheckOut.SuperMarketItemList
+{
+    /// <summary>
+    /// This class is responsible for define the Products or Items. @Author: Anirban Ghosh
+    /// </summary>
+    class Item
+    {
+        public string Sku { get; }
+        public PricingStrategy PricingStrategy { get; }
+
+        public Item(string sku, PricingStrategy pricingStrategy)
+        {
+            Sku = sku;
+            PricingStrategy = pricingStrategy;
+        }
+    }
+}
